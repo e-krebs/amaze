51202e75115d2c6ab1b011cb9b03e1eea3a16eb7
e-krebs <e-krebs@users.noreply.github.com>
2021-05-15 12:53:51 +0200

    grey background

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,22 @@ import { Maze } from '../components/Maze'
 import { MazeData } from '../interfaces'
 import { sampleMaze } from '../utils/sample-maze'
 import 'tailwindcss/tailwind.css'
+import { useEffect } from 'react'
 
 type Props = {
   maze: MazeData;
 }
 
-const IndexPage = ({ maze }: Props) => (
-  <main className="p-8">
-    <h1 className="text-4xl font-bold text-center py-8">aMaze</h1>
-    <Maze maze={maze} />
-  </main>
-)
+const IndexPage = ({ maze }: Props) => {
+  useEffect(() => { document.body.classList.add('bg-gray-50') });
 
+  return (
+    <main className="inline-flex flex-col items-center w-full p-8">
+      <h1 className="text-4xl font-bold py-8">aMaze</h1>
+      <Maze maze={maze} />
+    </main>
+  );
+}
 export const getStaticProps: GetStaticProps = async () => {
   const maze: MazeData = sampleMaze
   return { props: { maze } }
