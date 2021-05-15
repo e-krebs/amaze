c529c93de7bf356d688be848b02d9b8d33181369
e-krebs <e-krebs@users.noreply.github.com>
2021-05-15 13:00:55 +0200

    fix styles missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './utils/styles/*.ts'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
