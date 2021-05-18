9ad9df016f9d6ece8a592d950b9caed69812a09b
e-krebs <e-krebs@users.noreply.github.com>
2021-05-18 20:54:40 +0200

    implement maze generation

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,50 +1,42 @@
 import { Item } from '@react-stately/collections'
 import { useHotkeys } from 'react-hotkeys-hook'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import { Card } from '../components/Card'
 import { DropDown } from '../components/DropDown'
 import { KeyCap } from '../components/KeyCap'
 import { Maze } from '../components/Maze'
-import { AllSizes, Size } from '../interfaces'
-import { sampleMaze0, sampleMaze1, sampleMaze2, sampleMaze3 } from '../utils/sample-maze'
+import { AllSizes, Cols, Size } from '../interfaces'
 import { GameContext } from '../utils/contexts/GameContext'
+import { generate } from '../utils/generate'
+import { MazeData } from '../models/MazeData'
 
 const Game = () => {
-  const mazeList = [sampleMaze0, sampleMaze1, sampleMaze2, sampleMaze3];
-  const [mazeIndex, setMazeIndex] = useState(0);
-  const [maze, setMaze] = useState(mazeList[mazeIndex]);
-  const [hasWon, setHasWon] = useState(false);
   const [size, setSize] = useState<Size>('XL');
-  const [key, forceUpdate] = React.useState(0);
-
-  useEffect(() => {
-    if (mazeIndex > mazeList.length - 1) {
-      setHasWon(true);
-    } else {
-      setMaze(mazeList[mazeIndex]);
-    }
-  }, [mazeIndex]);
+  const [level, setLevel] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const hasWon = useMemo(() => level > 4, [level])
+  const maze: MazeData | null = useMemo(() => {
+    if (hasWon) return null;
+    setLoading(true);
+    const newMaze = generate(level * 2 + 3 as Cols);
+    setLoading(false);
+    return newMaze;
+  }, [level]);
 
   const reset = useCallback(() => {
-    setHasWon(false);
-    setMazeIndex(0);
-    forceUpdate(value => value + 1);
+    setLevel(0);
+    setLevel(level => level + 1);
   }, []);
 
   useHotkeys('escape', reset);
 
   return (
-    <GameContext.Provider value={{
-      level: mazeIndex + 1,
-      currentMaze: maze,
-      nextMaze: () => setMazeIndex(value => value + 1),
-      size,
-    }}>
+    <>
       <Card className="space-x-2">
         <div className="flex items-center space-x-2 pt-3 pb-6">
           <h2 className="flex-grow text-center text-2xl font-semibold">
-            {hasWon ? "You won 🎉" : `level ${mazeIndex + 1}`}
+            {hasWon ? "You won 🎉" : `level ${level}`}
           </h2>
           <DropDown
             defaultSelectedKey={size}
@@ -65,14 +57,31 @@ const Game = () => {
       {hasWon ? (
         <Card className="text-pink-400 text-lg">
           <p className="p-6">
-            You reached level {mazeIndex}
+            You reached level {level - 1}
             <br />Congrats, you won 🎉
           </p>
         </Card>
       ) : (
-        <Maze key={key} />
-      )}
-    </GameContext.Provider>
+        <>
+          {loading ? (
+              <Card className="text-green-400 text-lg">
+                <p className="p-6">Generating maze...</p>
+              </Card>
+          ) : (
+            <GameContext.Provider value={{
+              level,
+              currentMaze: maze!,
+              nextMaze: () => setLevel(level => level + 1),
+              size,
+            }}>
+              <Maze />
+            </GameContext.Provider>
+
+          )}
+        </>
+      )
+      }
+    </>
   );
 };
 
