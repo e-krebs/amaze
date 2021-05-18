5ca2e06d37f1dd7b69211a0621ba3b3165c83d08
e-krebs <e-krebs@users.noreply.github.com>
2021-05-18 20:51:49 +0200

    move Cols to interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,5 +2,7 @@ export const AllSizes = ['S', 'M', 'L', 'XL'] as const;
 
 export type Size = (typeof AllSizes)[number];
 
+export type Cols = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
 export type Direction = 'north' | 'east' | 'south' | 'west';
 export type Position = 'center' | Direction;
--- utils/styles/cols.ts
+++ utils/styles/cols.ts
@@ -1,4 +1,4 @@
-export type Cols = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+import { Cols } from "../../interfaces";
 
 export const gridCols: Record<Cols, string> = {
   0: 'grid-cols-none',
